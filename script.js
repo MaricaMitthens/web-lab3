70e8618fe87a83cd2d60d2a9d75d7454fed5566a
MaricaMithens <mari.aleo@yandex.ru>
2019-04-01 22:49:37 +0300

    first step in js

---
+++ script.js
@@ -0,0 +1,32 @@
+/*alert("hello");
+[1, 2].forEach(alert);
+var result = prompt("вопрос", 100);
+alert(result);
+var isAdmin = confirm("Вы - администратор?");
+
+alert(isAdmin);
+
+function checkAge(age) {
+    return (age > 18) || confirm('Родители разрешили?');
+}
+
+checkAge(105)*/
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+ctx.closePath();
