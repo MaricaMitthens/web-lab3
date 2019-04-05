50724bafb84eef8dae2c7abc3ad0c83d6ace4b70
MaricaMithens <mari.aleo@yandex.ru>
2019-04-05 23:16:20 +0300

    draw images and make it dark

--- script.js
+++ script.js
@@ -11,22 +11,63 @@ function checkAge(age) {
 }
 
 checkAge(105)*/
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+//var canvas = document.getElementById("myCanvas");
+//var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function draw(callback) {
+    var ctx = document.getElementById('canvas').getContext("2d");
+    var img = new Image();
+    var counter = 0;
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0);
+        counter++;
+        if (counter === 4) {
+            callback();
+        }
+    };
+
+    var img2 = new Image();
+    img2.onload = function () {
+        ctx.drawImage(img2, 300, 200);
+        counter++;
+        if (counter === 4) {
+            callback();
+        }
+    }
+
+    var img3 = new Image();
+    img3.onload = function () {
+        ctx.drawImage(img3, 0, 200);
+        counter++;
+        if (counter === 4) {
+            callback();
+        }
+    }
+
+    var img4 = new Image();
+    img4.onload = function () {
+        ctx.drawImage(img4, 300, 0);
+        counter++;
+        if (counter === 4) {
+            callback();
+        }
+    }
+    img.src = 'https://source.unsplash.com/collection/1127163/300x200';
+    img2.src = 'https://source.unsplash.com/collection/1127177/300x200?=1';
+    img3.src = 'https://source.unsplash.com/collection/1147624/300x200?=2';
+    img4.src = 'https://source.unsplash.com/collection/1065412/300x200?=3';
+
+}
+
+
+function drawBlack() {
+    var ctx = document.getElementById('canvas').getContext("2d");
+    ctx.rect(0, 0, 600, 400);
+    ctx.fillStyle = "rgba(0,0,0,0.5)";
+    ctx.fill();
+}
+
+draw(drawBlack);
+//drawBlack();
+
